1b56ac2ce3e7e269884e4f259b03fa11f283e367
Carlettos <kako3000@gmail.com>
2022-10-22 00:18:15 -0300

    removed polished bricks and standarized some code

--- src/main/java/com/carlettos/newwalls/NewBlocks.java
+++ src/main/java/com/carlettos/newwalls/NewBlocks.java
@@ -18,28 +18,11 @@ import net.minecraft.util.Pair;
 import net.minecraft.util.registry.Registry;
 
 public class NewBlocks {
-
     private static final LinkedList<Pair<Block, String>> BLOCKS = new LinkedList<>();
-    
     public static final Logger LOGGER = LoggerFactory.getLogger("block_creation");
     
-    public static final Block POLISHED_GRANITE_BRICKS = createBlock(Blocks.POLISHED_GRANITE, "bricks");
-    public static final Block POLISHED_GRANITE_BRICK_SLAB = createSlab(POLISHED_GRANITE_BRICKS);
-    public static final Block POLISHED_GRANITE_BRICK_STAIRS = createStairs(POLISHED_GRANITE_BRICKS);
-    public static final Block POLISHED_GRANITE_BRICK_WALL = createWall(POLISHED_GRANITE_BRICKS);
-    
-    public static final Block POLISHED_DIORITE_BRICKS = createBlock(Blocks.POLISHED_DIORITE, "bricks");
-    public static final Block POLISHED_DIORITE_BRICK_SLAB = createSlab(POLISHED_DIORITE_BRICKS);
-    public static final Block POLISHED_DIORITE_BRICK_STAIRS = createStairs(POLISHED_DIORITE_BRICKS);
-    public static final Block POLISHED_DIORITE_BRICK_WALL = createWall(POLISHED_DIORITE_BRICKS);
-
-    public static final Block POLISHED_ANDESITE_BRICKS = createBlock(Blocks.POLISHED_ANDESITE, "bricks");
-    public static final Block POLISHED_ANDESITE_BRICK_SLAB = createSlab(POLISHED_ANDESITE_BRICKS);
-    public static final Block POLISHED_ANDESITE_BRICK_STAIRS = createStairs(POLISHED_ANDESITE_BRICKS);
-    public static final Block POLISHED_ANDESITE_BRICK_WALL = createWall(POLISHED_ANDESITE_BRICKS);
-    
-    public static final Block QUARTZ_WALL = createWall(Blocks.QUARTZ_BLOCK, "quartz_wall");
-    public static final Block PURPUR_WALL = createWall(Blocks.PURPUR_BLOCK, "purpur_wall");
+    public static final Block QUARTZ_BLOCK_WALL = createWall(Blocks.QUARTZ_BLOCK);
+    public static final Block PURPUR_BLOCK_WALL = createWall(Blocks.PURPUR_BLOCK);
     public static final Block PRISMARINE_BRICK_WALL = createWall(Blocks.PRISMARINE_BRICKS);
     public static final Block DARK_PRISMARINE_WALL = createWall(Blocks.DARK_PRISMARINE);
     public static final Block SMOOTH_SANDSTONE_WALL = createWall(Blocks.SMOOTH_SANDSTONE);
@@ -55,62 +38,45 @@ public class NewBlocks {
     public static final Block CUT_SANDSTONE_WALL = createWall(Blocks.CUT_SANDSTONE);
     public static final Block CUT_RED_SANDSTONE_STAIRS = createStairs(Blocks.CUT_RED_SANDSTONE);
     public static final Block CUT_RED_SANDSTONE_WALL = createWall(Blocks.CUT_RED_SANDSTONE);
-    
-    public static final Block CRACKED_STONE_BRICK_SLAB = createSlab(Blocks.CRACKED_STONE_BRICKS);
     public static final Block CRACKED_STONE_BRICK_STAIRS = createStairs(Blocks.CRACKED_STONE_BRICKS);
+    public static final Block CRACKED_STONE_BRICK_SLAB = createSlab(Blocks.CRACKED_STONE_BRICKS);
     public static final Block CRACKED_STONE_BRICK_WALL = createWall(Blocks.CRACKED_STONE_BRICKS);
-
-    public static final Block CRACKED_NETHER_BRICK_SLAB = createSlab(Blocks.CRACKED_NETHER_BRICKS);
     public static final Block CRACKED_NETHER_BRICK_STAIRS = createStairs(Blocks.CRACKED_NETHER_BRICKS);
+    public static final Block CRACKED_NETHER_BRICK_SLAB = createSlab(Blocks.CRACKED_NETHER_BRICKS);
     public static final Block CRACKED_NETHER_BRICK_WALL = createWall(Blocks.CRACKED_NETHER_BRICKS);
     public static final Block CRACKED_NETHER_BRICK_FENCE = createFence(Blocks.CRACKED_NETHER_BRICKS);
     public static final Block CRACKED_NETHER_BRICK_FENCE_GATE = createFenceGate(Blocks.CRACKED_NETHER_BRICKS);
-    
+    public static final Block NETHER_BRICK_FENCE_GATE = createFenceGate(Blocks.NETHER_BRICKS);
     public static final Block RED_NETHER_BRICK_FENCE = createFence(Blocks.RED_NETHER_BRICKS);
     public static final Block RED_NETHER_BRICK_FENCE_GATE = createFenceGate(Blocks.RED_NETHER_BRICKS);
-    public static final Block NETHER_BRICK_FENCE_GATE = createFenceGate(Blocks.NETHER_BRICKS);
-    
-    public static final Block CRACKED_POLISHED_BLACKSTONE_BRICK_SLAB = createSlab(Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS);
     public static final Block CRACKED_POLISHED_BLACKSTONE_BRICK_STAIRS = createStairs(Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS);
+    public static final Block CRACKED_POLISHED_BLACKSTONE_BRICK_SLAB = createSlab(Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS);
     public static final Block CRACKED_POLISHED_BLACKSTONE_BRICK_WALL = createWall(Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS);
-    
-    public static final Block QUARTZ_BRICK_SLAB = createSlab(Blocks.QUARTZ_BRICKS);
     public static final Block QUARTZ_BRICK_STAIRS = createStairs(Blocks.QUARTZ_BRICKS);
+    public static final Block QUARTZ_BRICK_SLAB = createSlab(Blocks.QUARTZ_BRICKS);
     public static final Block QUARTZ_BRICK_WALL = createWall(Blocks.QUARTZ_BRICKS);
     public static final Block QUARTZ_BRICK_FENCE = createFence(Blocks.QUARTZ_BRICKS);
     public static final Block QUARTZ_BRICK_FENCE_GATE = createFenceGate(Blocks.QUARTZ_BRICKS);
-    
-    public static final Block CRACKED_DEEPSLATE_BRICK_SLAB = createSlab(Blocks.CRACKED_DEEPSLATE_BRICKS);
     public static final Block CRACKED_DEEPSLATE_BRICK_STAIRS = createStairs(Blocks.CRACKED_DEEPSLATE_BRICKS);
+    public static final Block CRACKED_DEEPSLATE_BRICK_SLAB = createSlab(Blocks.CRACKED_DEEPSLATE_BRICKS);
     public static final Block CRACKED_DEEPSLATE_BRICK_WALL = createWall(Blocks.CRACKED_DEEPSLATE_BRICKS);
-    
-    public static final Block CRACKED_DEEPSLATE_TILE_SLAB = createSlab(Blocks.CRACKED_DEEPSLATE_TILES);
     public static final Block CRACKED_DEEPSLATE_TILE_STAIRS = createStairs(Blocks.CRACKED_DEEPSLATE_TILES);
+    public static final Block CRACKED_DEEPSLATE_TILE_SLAB = createSlab(Blocks.CRACKED_DEEPSLATE_TILES);
     public static final Block CRACKED_DEEPSLATE_TILE_WALL = createWall(Blocks.CRACKED_DEEPSLATE_TILES);
-    
-    public static final Block TUFF_SLAB = createSlab(Blocks.TUFF);
     public static final Block TUFF_STAIRS = createStairs(Blocks.TUFF);
+    public static final Block TUFF_SLAB = createSlab(Blocks.TUFF);
     public static final Block TUFF_WALL = createWall(Blocks.TUFF);
-    
-    public static final Block CALCITE_SLAB = createSlab(Blocks.CALCITE);
     public static final Block CALCITE_STAIRS = createStairs(Blocks.CALCITE);
+    public static final Block CALCITE_SLAB = createSlab(Blocks.CALCITE);
     public static final Block CALCITE_WALL = createWall(Blocks.CALCITE);
-    
-    public static final Block SMOOTH_BASALT_SLAB = createSlab(Blocks.SMOOTH_BASALT);
     public static final Block SMOOTH_BASALT_STAIRS = createStairs(Blocks.SMOOTH_BASALT);
+    public static final Block SMOOTH_BASALT_SLAB = createSlab(Blocks.SMOOTH_BASALT);
     public static final Block SMOOTH_BASALT_WALL = createWall(Blocks.SMOOTH_BASALT);
-    
     public static final Block SCULK_STAIRS = createStairs(Blocks.SCULK);
     public static final Block SCULK_SLAB = createSlab(Blocks.SCULK);
     public static final Block SCULK_WALL = createWall(Blocks.SCULK);
     public static final Block SCULK_FENCE = createFence(Blocks.SCULK);
     public static final Block SCULK_FENCE_GATE = createFenceGate(Blocks.SCULK);
-    
-    public static final Block createBlock(Block parent, String type) {
-        Block block = new Block(Settings.copy(parent));
-        common(parent, block, type);
-        return block;
-    }
 
     public static final SlabBlock createSlab(Block parent) {
         SlabBlock slab = new SlabBlock(Settings.copy(parent));
@@ -122,12 +88,6 @@ public class NewBlocks {
         StairsBlock stairs = new StairsBlock(parent.getDefaultState(), Settings.copy(parent));
         common(parent, stairs, "stairs");
         return stairs;
-    }
-    
-    public static final WallBlock createWall(Block parent, String name) {
-        WallBlock wall = new WallBlock(Settings.copy(parent));
-        BLOCKS.add(new Pair<>(wall, name));
-        return wall;
     }
     
     public static final WallBlock createWall(Block parent) {
