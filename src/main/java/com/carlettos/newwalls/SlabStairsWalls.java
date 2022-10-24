9cf0b204fc706e323dd99eca6de06fcb96f50965
Carlettos <kako3000@gmail.com>
2022-10-24 00:45:20 -0300

    Added a lot of blocks, including all wool, terracotta and concrete variants

--- src/main/java/com/carlettos/newwalls/SlabStairsWalls.java
+++ src/main/java/com/carlettos/newwalls/SlabStairsWalls.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ShearsItem;
 
 public class SlabStairsWalls implements ModInitializer {
     public static final String MOD_ID = "newwalls";
